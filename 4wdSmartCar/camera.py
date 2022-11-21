d09765a6b6f28b5333821e3b554f65b3c454fc4f
luvisiki <liuqianrong1323@gmail.com>
2022-11-21 19:38:04 +0800

    update

--- 4wdSmartCar/camera.py
+++ 4wdSmartCar/camera.py
@@ -7,20 +7,22 @@ def camera_catch():
     capture = cv2.VideoCapture(0)
     while 1:
         ret, frame = capture.read()
+        fps = capture.get(cv2.CAP_PROP_FPS)
+        print(fps)
         frame = imutils.resize(frame, height=320, width=480)
-        # 高斯模糊
+        # 高斯模糊(GaussianBlur)
         blurr = cv2.GaussianBlur(frame, (11, 11), 0)
-        # 转换为hsv空间颜色
+        # 转换为hsv空间颜色 (change into hsv space)
         hsv = cv2.cvtColor(blurr, cv2.COLOR_BGR2HSV)
-        # 进行颜色锁定（黑色）
+        # 进行颜色锁定（黑色）(focus black)
         lower_hsv = np.array([0, 0, 0])
         higer_hsv = np.array([180, 255, 46])
-        # 二值化处理
+        # 二值化处理(Binary processing)
         mask = cv2.inRange(hsv, lower_hsv, higer_hsv)
-        # # 腐蚀操作
-        # mask = cv2.erode(mask, None, iterations=2)
-        # # 膨胀操作
-        # mask = cv2.dilate(mask, None, iterations=2)
+        # 腐蚀操作（Corrosion operation）
+        mask = cv2.erode(mask, None, iterations=2)
+        # 膨胀操作 （Expansion operation）
+        mask = cv2.dilate(mask, None, iterations=2)
         cv2.imshow('test', mask)
         # print(mask[0])
         c = cv2.waitKey(1)
