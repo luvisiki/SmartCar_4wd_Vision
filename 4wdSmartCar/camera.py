677c3a460be492ab2bc650aa6798f5d7e0c77885
luvisiki <liuqianrong1323@gmail.com>
2022-11-25 00:11:18 +0800

    fix bug

--- 4wdSmartCar/camera.py
+++ 4wdSmartCar/camera.py
@@ -1,32 +1,33 @@
 import cv2
-import multiprocessing
+from multiprocessing import Process,Queue
 import threading
 import numpy as np
 import time
 import Motor
-from queue import Queue
+
+
 # @@
 # @
 # @
 # @@
 # global cx
-cx = 0
+
+# cx = 0
 lock = threading.Lock
 
-def camera_catch_color(car):
-    global cx
-
+def camera_catch_color(car,q):
+    # global cx
+    global frame
     capture = cv2.VideoCapture(0)
     capture.set(3, 320)
     capture.set(4, 240)  # 像素高度？
-
-    # time.sleep(5)
-    while 1:
-
+    
+    while True:
+        
         ret, frame = capture.read()
 
         # frame = cv2.imread('./pic/left_angle.jpg')
-        frame = frame[80:240, :]  # 320*240，只取高度的下半部分，减小计算量，较少其他因素
+        frame = frame[120:240, :]  # 320*240，只取高度的下半部分，减小计算量，较少其他因素
         blurr = cv2.GaussianBlur(frame, (11, 11), 0)
         # 转换为hsv空间颜色
         hsv = cv2.cvtColor(blurr, cv2.COLOR_BGR2HSV)
@@ -61,61 +62,69 @@ def camera_catch_color(car):
 
 
             # print(cx, cy)
-            cv2.line(frame, (cx, 0), (cx, 10000), (255, 0, 0), 1)
-            cv2.line(frame, (0, cy), (10000, cy), (255, 0, 0), 1)
+            cv2.line(frame, (cx, 0), (cx, 320), (255, 0, 0), 1)
+            cv2.line(frame, (0, cy), (240, cy), (255, 0, 0), 1)
             cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
         else:
-            
+            print('there is no black lines')
             car.Motor_init()
             # car.SmartCar_turn_Right(30,30,2)
-        cv2.waitKey(1)
+        cv2.waitKey(25)
         cv2.imshow('test', frame)
-    # cv2.imshow('test', frame)
-    # return cx, cy, frame
+        q.put(cx)
+        # cv2.waitKey(1)
+        
+    
 
-
-def Motor_State(c):
-    global cx
-    print(cx)
-    if cx >= 200 and cx <= 270:
-        print("偏右")
-        c.SmartCar_turn_Right(30, 20, 0.3)
-        c.Motor_init()
-        # time.sleep(0.5)
-        # print(cv2.CAP_PROP_FPS)
-    if cx > 270:
-        c.SmartCar_turn_Right(40,20,0.2)
-        c.Motor_init()
-    if cx <= 200 and cx > 120:
-        print("直走")
-        c.SmartCar_run(30, 0.3)
-        c.Motor_init()
-        # print(cv2.CAP_PROP_FPS)
-    if cx <= 120 and cx > 50:
-        print("偏左")
-        c.SmartCar_turn_Left(30, 20, 0.3)
-        # print(LEFT_MOTOR_BACK)
-        c.Motor_init()
-        # time.sleep(0.5)
-        # print(cv2.CAP_PROP_FPS)
-    if cx <= 50:
-        c.SmartCar_turn_Left(40,20,0.2)
-        c.Motor_init()
+def Motor_State(c,q):
+    # global cx
+    # print(cx)
+    while True:
+        point = q.get()
+        print(point)
+        if point >= 200 and point <= 270:
+            print("偏右")
+            c.SmartCar_turn_Right(30, 20, 0.3)
+            c.Motor_init()
+            # time.sleep(0.5)
+            # print(cv2.CAP_PROP_FPS)
+        if point > 270:
+            c.SmartCar_turn_Right(40,20,0.2)
+            c.Motor_init()
+        if point <= 200 and point > 120:
+            print("直走")
+            c.SmartCar_run(30, 0.3)
+            c.Motor_init()
+            # print(cv2.CAP_PROP_FPS)
+        if point <= 120 and point > 50:
+            print("偏左")
+            c.SmartCar_turn_Left(30, 20, 0.3)
+            # print(LEFT_MOTOR_BACK)
+            c.Motor_init()
+            # time.sleep(0.5)
+            # print(cv2.CAP_PROP_FPS)
+        if point <= 50:
+            c.SmartCar_turn_Left(40,20,0.2)
+            c.Motor_init()
     
 
 
 if __name__ == '__main__':
     # global cx, cy, frame
-    lock = threading.Lock()
     try:
         c = Motor.MotorControl()
         c.Motor_init()
-        
+        q = Queue()
 
-        t1 = threading.Thread(target=camera_catch_color, args=(c,))
-        t2 = threading.Thread(target=Motor_State,args=(c,))
+        t1 = threading.Thread(target=camera_catch_color, args=(c,q))
+        t2 = threading.Thread(target=Motor_State,args=(c,q))
+        
+        # t1 = Process(target=camera_catch_color,args=(c,q))
+        # t2 = Process(target=Motor_State,args=(c,q))
+        # t3 = Process(target=Camera_show,args=(q,))
         t1.start()
         t2.start()
+        
         # while 1:
         #     # cx,cy,frame = camera_catch_color(capture)
         #     Motor_State(c)
