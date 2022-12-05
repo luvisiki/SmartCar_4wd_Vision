2ad8dc871ab84779b5419cb8c4e84644f7ba094b
luvisiki <liuqianrong1323@gmail.com>
2022-12-05 17:52:02 +0800

    update

--- 4wdSmartCar/Motor.py
+++ 4wdSmartCar/Motor.py
@@ -1,57 +1,90 @@
 import wiringpi
 import time
+import RPi.GPIO as GPIO
+
 
 
 class MotorControl:
-
+    '''
+    @@@@@@@@
+    @@ Developer: Han Cao , id:202244060101
+    @@ Class name: MotorControl 
+    @@ Input: none
+    @@ output:fuction
+    @@ Description:Class encapsulates the initialization function of the motor, the variate drive function, and the ultrasonic distance measurement function.
+    @@@@@@@@
+    '''
     def __init__(self):
         wiringpi.wiringPiSetup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         self.LEFT_MOTOR_FONT = 28
         self.LEFT_MOTOR_BACK = 29
         self.RIGHT_MOTOR_BACK = 25
         self.RIGHT_MOTOR_FONT = 24
         self.LEFT_MOTOR_PWMcontorl = 27
         self.RIGHT_MOTOR_PWMcontorl = 23
-        self.EchoPin = 30   # Send
-        self.TrigPin = 31   # Receive
+
+        # using BCM coder
+        self.EchoPin = 0   # ULsonic Send
+        self.TrigPin = 1   # ULsonic Receive
         # self.SERVO_PIN = 4
 
         self.LOW = 0
         self.HIGH = 1
 
     def Motor_init(self):
+        '''
+        @@@@@@@@
+        @@ Developer: Han Cao, id:202244060101
+        @@ Fuction name: Motor_init
+        @@ Input: none
+        @@ output: none
+        @@ Description: Using wiringpi Initialization Motor Pin and setup softPwm. Using GPIO Initialization Ultrasonic PIN.
+        @@@@@@@@
+        '''
         wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.LOW)
         wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.LOW)
         wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.LOW)
         wiringpi.pinMode(self.RIGHT_MOTOR_BACK, self.LOW)
-
-        wiringpi.pinMode(self.EchoPin, self.HIGH)
-        wiringpi.pinMode(self.TrigPin, self.LOW)
-
+        GPIO.setup(self.EchoPin, GPIO.IN)
+        GPIO.setup(self.TrigPin, GPIO.OUT)
         wiringpi.softPwmCreate(self.LEFT_MOTOR_PWMcontorl, 0, 100)
         wiringpi.softPwmCreate(self.RIGHT_MOTOR_PWMcontorl, 0, 100)
-        # wiringpi.softPwmCreate(self.SERVO_PIN, 0, 50)
-        # wiringpi.softPwmWrite(self.SERVO_PIN,self.LOW)
+        
 
     def ultarsonic_ExaminDistant(self):
-        # GPIO.output(TrigPin, GPIO.HIGH)
-        wiringpi.digitalWrite(self.TrigPin,self.HIGH)
+        '''
+        @@@@@@@@
+        @@ Developer: Han Cao, id:202244060101
+        @@ Fuction name: ultarsonic_ExaminDistant
+        @@ Input: none
+        @@ output: Distance measurements
+        @@ Description: Use the ultrasonic principle to detect the high level time of the EchoPin time.Then use the formula to calculate the distance from the obstacle.
+        @@@@@@@@
+        '''
+        GPIO.output(self.TrigPin, GPIO.HIGH)
         time.sleep(0.000015)
-        # GPIO.output(TrigPin, GPIO.LOW)
-        wiringpi.digitalWrite(self.TrigPin,self.LOW)
-
-        while not wiringpi.digitalRead(self.EchoPin):
+        GPIO.output(self.TrigPin, GPIO.LOW)
+        while not GPIO.input(self.EchoPin):
             pass
         t1 = time.time()
-        while wiringpi.digitalRead(self.EchoPin):
+        while GPIO.input(self.EchoPin):
             pass
         t2 = time.time()
-
-        # print("distance is %d " % (((t2 - t1) * 340 / 2) * 100))
+        # print ("distance is %d " % (((t2 - t1) * 340 / 2) * 100))
         time.sleep(0.01)
-        return ((t2 - t1) * 340 / 2) * 100
+        return int(((t2 - t1) * 340 / 2) * 100)
 
     def SmartCar_run(self, speed, delay):
+        '''
+        @@@@@@@@
+        @@ Developer: Han Cao, id:202244060101
+        @@ Fuction name: SmartCar_run
+        @@ Input: Para1-> speed Para2->delay time
+        @@ Description: drive the pin of motor and make 4WD car go straight
+        @@@@@@@@
+        '''
         wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.HIGH)
         wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.HIGH)
         wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.LOW)
@@ -64,6 +97,14 @@ class MotorControl:
         self.Motor_stop()
 
     def SmartCar_back(self, speed1, speed2, delay):
+        '''
+        @@@@@@@@
+        @@ Developer: Han Cao, id:202244060101
+        @@ Fuction name: SmartCar_back
+        @@ Input: Para1-> speed1(left motor) Para2->speed2(right motor) Para3->delay time
+        @@ Description: drive the pin of motor and make 4WD car Stand back. 
+        @@@@@@@@
+        '''
         wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.LOW)
         wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.LOW)
         wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.HIGH)
@@ -75,28 +116,74 @@ class MotorControl:
         time.sleep(delay)
         self.Motor_stop()
 
-    def SmartCar_turn_Left(self, speed1, speed2, delay):
-        wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.LOW)
-        wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.HIGH)
-        wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.HIGH)
-        wiringpi.pinMode(self.RIGHT_MOTOR_BACK, self.LOW)
-        wiringpi.digitalWrite(self.RIGHT_MOTOR_FONT, self.HIGH)
-        wiringpi.digitalWrite(self.LEFT_MOTOR_BACK, self.HIGH)
-        wiringpi.softPwmWrite(self.LEFT_MOTOR_PWMcontorl, speed2)
-        wiringpi.softPwmWrite(self.RIGHT_MOTOR_PWMcontorl, speed1)
-        time.sleep(delay)
+    def SmartCar_turn_Left(self, speed1, speed2, mode, delay):
+        '''
+        @@@@@@@@
+        @@ Developer: Han Cao, id:202244060101
+        @@ Fuction name: SmartCar_turn_Left
+        @@ Input: 
+        @@          Para1-> speed1:(0-100) 
+        @@          Para2-> speed2:(0-100)
+        @@          Para3-> mode:(1 or 2) 1: left and right motor both in same direction 2: different direction
+        @@ Description: Drive 4WDcar turn left , if mode is 1 ,using different speed(speed1>speed2) Make the car turn a corner with a small angle.
+            if mode is 2. Make the car turn a corner with a sharp angle
+        @@@@@@@@
+        '''
+        if mode == 1:
+            wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.LOW)
+            wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.HIGH)
+            wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.HIGH)
+            wiringpi.pinMode(self.RIGHT_MOTOR_BACK, self.LOW)
+            wiringpi.digitalWrite(self.RIGHT_MOTOR_FONT, self.HIGH)
+            wiringpi.digitalWrite(self.LEFT_MOTOR_BACK, self.HIGH)
+            wiringpi.softPwmWrite(self.LEFT_MOTOR_PWMcontorl, speed2)
+            wiringpi.softPwmWrite(self.RIGHT_MOTOR_PWMcontorl, speed1)
+            time.sleep(delay)
+        elif mode == 2:
+            wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.HIGH)
+            wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.HIGH)
+            wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.LOW)
+            wiringpi.pinMode(self.RIGHT_MOTOR_BACK, self.LOW)
+            wiringpi.digitalWrite(self.RIGHT_MOTOR_FONT, self.HIGH)
+            wiringpi.digitalWrite(self.LEFT_MOTOR_BACK, self.HIGH)
+            wiringpi.softPwmWrite(self.LEFT_MOTOR_PWMcontorl, speed2)
+            wiringpi.softPwmWrite(self.RIGHT_MOTOR_PWMcontorl, speed1)
+            time.sleep(delay)
         self.Motor_stop()
 
-    def SmartCar_turn_Right(self, speed1, speed2, delay):
-        wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.HIGH)
-        wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.LOW)
-        wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.LOW)
-        wiringpi.pinMode(self.RIGHT_MOTOR_BACK, self.HIGH)
-        wiringpi.digitalWrite(self.LEFT_MOTOR_FONT, self.HIGH)
-        wiringpi.digitalWrite(self.RIGHT_MOTOR_BACK, self.HIGH)
-        wiringpi.softPwmWrite(self.LEFT_MOTOR_PWMcontorl, speed1)
-        wiringpi.softPwmWrite(self.RIGHT_MOTOR_PWMcontorl, speed2)
-        time.sleep(delay)
+    def SmartCar_turn_Right(self, speed1, speed2, mode, delay):
+        '''
+        @@@@@@@@
+        @@ Developer: Han Cao, id:202244060101
+        @@ Fuction name: SmartCar_turn_Right
+        @@ Input: 
+        @@          Para1-> speed1:(0-100) 
+        @@          Para2-> speed2:(0-100)
+        @@          Para3-> mode:(1 or 2) 1: left and right motor both in same direction 2: different direction
+        @@ Description: Drive 4WDcar turn left , if mode is 1 ,using different speed(speed1>speed2) Make the car turn a corner with a small angle.
+            if mode is 2. Make the car turn a corner with a sharp angle
+        @@@@@@@@
+        '''
+        if mode == 1:
+            wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.HIGH)
+            wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.LOW)
+            wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.LOW)
+            wiringpi.pinMode(self.RIGHT_MOTOR_BACK, self.HIGH)
+            wiringpi.digitalWrite(self.LEFT_MOTOR_FONT, self.HIGH)
+            wiringpi.digitalWrite(self.RIGHT_MOTOR_BACK, self.HIGH)
+            wiringpi.softPwmWrite(self.LEFT_MOTOR_PWMcontorl, speed1)
+            wiringpi.softPwmWrite(self.RIGHT_MOTOR_PWMcontorl, speed2)
+            time.sleep(delay)
+        elif mode == 2:
+            wiringpi.pinMode(self.LEFT_MOTOR_FONT, self.HIGH)
+            wiringpi.pinMode(self.RIGHT_MOTOR_FONT, self.HIGH)
+            wiringpi.pinMode(self.LEFT_MOTOR_BACK, self.LOW)
+            wiringpi.pinMode(self.RIGHT_MOTOR_BACK, self.LOW)
+            wiringpi.digitalWrite(self.LEFT_MOTOR_FONT, self.HIGH)
+            wiringpi.digitalWrite(self.RIGHT_MOTOR_FONT, self.HIGH)
+            wiringpi.softPwmWrite(self.LEFT_MOTOR_PWMcontorl, speed1)
+            wiringpi.softPwmWrite(self.RIGHT_MOTOR_PWMcontorl, speed2)
+            time.sleep(delay)
         self.Motor_stop()
 
     # def ServoAngle(self, pos):
@@ -105,15 +192,21 @@ class MotorControl:
     #     wiringpi.softPwmWrite(self.SERVO_PIN, int(2.5 + 10 * pos/180))
 
     def Motor_stop(self):
+        '''
+        @@@@@@@@
+        @@ Developer: Han Cao, id:202244060101
+        @@ Fuction name: Motor_stop
+        @@ Input: none 
+        @@ Description: stop drive the 4WDcar
+        @@@@@@@@
+        '''
         wiringpi.digitalWrite(self.LEFT_MOTOR_FONT, self.LOW)
         wiringpi.digitalWrite(self.RIGHT_MOTOR_FONT, self.LOW)
         wiringpi.digitalWrite(self.LEFT_MOTOR_BACK, self.LOW)
         wiringpi.digitalWrite(self.RIGHT_MOTOR_BACK, self.LOW)
-        # wiringpi.digitalWrite(self.SERVO_PIN, self.LOW)
         wiringpi.softPwmWrite(self.LEFT_MOTOR_PWMcontorl, 0)
         wiringpi.softPwmWrite(self.RIGHT_MOTOR_PWMcontorl, 0)
-        # wiringpi.softPwmWrite(self.SERVO_PIN,self.LOW)
-
+        
 
 # if __name__ == '__main__':
 #     try:
---
+++ 4wdSmartCar/main.py
@@ -0,0 +1,241 @@
+import cv2
+from multiprocessing import Process, Queue
+import numpy as np
+import threading
+import Motor
+import pid
+import time
+import RPi.GPIO as GPIO
+
+
+
+
+
+def camera_catch_color(car, q):
+
+    '''
+    @@@@@@@@
+    @@ Developer: QianRong LIU , id:202244060130
+    @@ Function name: camera_catch_color 
+    @@ Input: An instance of the class , An instance of the Queue
+    @@ output: cx, cy, frame, flag, mode
+    @@ Description: loop the frame. using Gaussian blur, Corrosion, Expansion:Morphological operation to Remove the noise of the current frame and And convert the frame into an HSV space for binary operation.
+    @@@@@@@@
+    '''
+
+    capture = cv2.VideoCapture(0)   # An instance of the video0
+    # set frame's Height and Length
+    capture.set(3, 320)  # CV_CAP_PROP_FRAME_WIDTH=3   帧宽度
+    capture.set(4, 240)  # CV_CAP_PROP_FRAME_HEIGHT=4  帧高度
+
+    while True:
+        ret, frame = capture.read()
+
+        
+        blurr = cv2.GaussianBlur(frame, (13, 13), 0)
+        # 转换为hsv空间颜色 turn into hsv space color
+        hsv = cv2.cvtColor(blurr, cv2.COLOR_BGR2HSV)
+        # 进行颜色锁定（黑色）hsv focus in black color
+        lower_hsv = np.array([0, 0, 0])
+        higer_hsv = np.array([180, 255, 46])
+        # turn mask into 255 or 0
+        mask = cv2.inRange(hsv, lower_hsv, higer_hsv)
+
+        # 腐蚀操作 Corrosion operation
+        mask = cv2.erode(mask, None, iterations=2)
+        # 膨胀操作 Expansion operation
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        flag, mode = Count255(car, mask)
+        print("falg: ", flag)
+
+        # @ Find the contours of the frame
+        # @ cv2.findcontours args**: img, mode, method
+        # @ cv2.RETR_EXTERNAL:只检测外轮廓
+        # @ cv2.CHAIN_APPROX_NONE:存储所有的轮廓点，相邻的两个点的像素位置差不超过1
+        contours, hierarchy = cv2.findContours(
+            mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if len(contours) > 0:
+            # cv2.contourArea轮廓面积，返回面积最大的轮廓 return biggest
+            c = max(contours, key=cv2.contourArea)
+            M = cv2.moments(c)  # cv2.moments计算图像矩，然后通过图象矩计算质心
+
+            # 几何中心的数学表示方式则(cx,cy)就是质心坐标 
+            # (cx,cy) means Mass coordinates
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+
+            # print(cx, cy)
+            cv2.line(frame, (cx, 0), (cx, 320), (255, 0, 0), 1)
+            cv2.line(frame, (0, cy), (240, cy), (255, 0, 0), 1)
+            cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
+        else:
+            print('there is no black lines')
+            cx = 0
+            cy = 0
+            # time.sleep(0.1)
+            # car.Motor_stop()
+            # car.SmartCar_turn_Right(30,30,2)
+        cv2.waitKey(10)
+        # cv2.imshow('test', frame)
+        q.put((cx, cy, frame, flag, mode))
+        # attention：
+        #   when using cv2.videocapture read, there has lots of index haven't been handle in time
+        #   using q.qsize()to make sure get the newest frame and handle it.
+        #####
+        length = q.qsize()
+        # print('Queue length : ', length)
+        if length > 5:
+            for i in range(length-5):
+                cx, cy, frame, flag, mode = q.get()
+    capture.release()
+
+
+def Motor_State(c, q):
+
+    while True:
+        pointX, pointY, frame, flag, mode = q.get()
+        # print(mode)
+        if mode == 2:
+            c.Motor_stop()
+            c.Motor_init()
+            print("There is something in front of 4WDCar, Stop the car.")
+
+        elif mode == 1:
+
+            if flag == 1:
+
+                c.SmartCar_turn_Left(40, 5, 1, 0.5)
+                # time.sleep(1)
+                print("right angle(left)")
+            elif flag == 2:
+                c.SmartCar_turn_Right(40, 5, 1, 0.5)
+                # time.sleep(1)
+                print("right angle(right)")
+            elif flag == 3:
+                c.SmartCar_turn_Left(40, 0, 2, 1.7)
+                print("Sharp angle(left)")
+            elif flag == 4:
+                c.SmartCar_turn_Right(40, 0, 2, 1.7)
+                print("Sharp angle(right)")
+            elif flag == 5:
+                c.SmartCar_turn_Left(25, 10, 1, 0.1)
+                print("Turn left")
+            elif flag == 6:
+                c.SmartCar_turn_Right(25, 10, 1, 0.1)
+                print("Turn right")
+
+        elif mode == 0:
+
+            if pointX == 0 and pointY == 0:
+                # c.Motor_stop()
+                print("Back")
+                c.SmartCar_back(20, 20, 0.1)
+                c.Motor_stop()
+                time.sleep(0.5)
+
+            elif (0 < pointX < 120):
+                c.SmartCar_turn_Left(25, 10, 1, 0.1)
+                print("Turn left")
+                # time.sleep(0.001)
+
+            elif (180 < pointX < 320):
+                c.SmartCar_turn_Right(25, 10, 1, 0.1)
+                print("Turn Right")
+                # time.sleep(0.001)
+
+            elif (pointX >= 120 and pointX <= 200):
+                print("GO Straight")
+                c.SmartCar_run(15, 0.1)
+                # time.sleep(0.001)
+
+                c.Motor_stop()
+
+
+def Count255(c, frame):
+    # frame 's cols
+    line1 = 40
+    line2 = 119
+    line3 = 200
+
+    Value1 = frame[line1]
+    Value2 = frame[line2]
+    Value3 = frame[line3]
+    Value2_Left = frame[line2, :160]
+    Value2_Right = frame[line2, 160:320]
+    Value3_Left = frame[line3, :160]
+    Value3_Right = frame[line3, 160:320]
+    count1 = len(np.where(Value1 == 255)[0])
+    count2 = len(np.where(Value2 == 255)[0])
+    count2_left = len(np.where(Value2_Left)[0])
+    count2_right = len(np.where(Value2_Right)[0])
+    count3 = len(np.where(Value3)[0])
+    count3_left = len(np.where(Value3_Left)[0])
+    count3_right = len(np.where(Value3_Right)[0])
+    cm = c.ultarsonic_ExaminDistant()
+    print("distant:", cm)
+    mode = 0
+    flag=0
+
+
+    
+    if cm <= 20:
+        mode = 2
+    '''
+    flag1:Solve the right-angle bending (turn left)
+    flag2:Solve the right-angle bending (turn right)
+    flag3:Solve the sharp angle bending (turn left)
+    flag4:Solve the sharp angle bending (turn right)
+    flag5:solve the angle shifting *left shift
+    flag6:solve the angle shifting *right shift
+    '''
+    if mode != 2:
+        
+        if count1 == 0 and count2 == 0 and (count3_left, count3_right != 0) and (count3_left > count3_right) and count3_left > 80:
+            flag = 1
+            mode = 1
+        elif count1 == 0 and count2 == 0 and (count3_left, count3_right != 0) and (count3_left < count3_right) and count3_right > 80:
+            flag = 2
+            mode = 1
+        elif count1 == 0 and count3 != 0 and (count2_left > count2_right) and count2_left > 60:
+            flag = 3
+            mode = 1
+        elif count1 == 0 and count3 != 0 and (count2_right > count2_left) and count2_right > 60:
+            flag = 4
+            mode = 1
+        elif count1 != 0 and (count2_left > count2_right) and count2_left > 40:
+            flag = 5
+            mode = 1
+        elif count1 != 0 and (count2_right > count2_left) and count2_right > 40:
+            flag = 6
+            mode = 1
+        else:
+            flag = 0
+            mode = 0
+    return flag, mode
+
+
+if __name__ == '__main__':
+    # global cx, cy, frame
+    # lock = threading.RLock()
+    try:
+        c = Motor.MotorControl()
+        c.Motor_init()
+        q = Queue()
+
+        t1 = threading.Thread(target=camera_catch_color, args=(c, q))
+        t2 = threading.Thread(target=Motor_State, args=(c, q))
+        # t2 = Process(target=Motor_State, args=(c, q))
+        t1.start()
+        t2.start()
+        Motor_State(c, q)
+
+        # t2.start()
+
+    except KeyboardInterrupt:
+        pass
+        c.Motor_stop()
+        c.Motor_init()
+        GPIO.cleanup()
+        t1.join()
+        t2.join()
